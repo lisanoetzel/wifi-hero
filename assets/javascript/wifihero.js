d33a26dbbf9bd9a0bac91525f4fb4e9930a68170
Jeffrey Layfield <jlayfield85@gmail.com>
2019-07-20 14:12:41 -0400

    javascript work july 20 2019

--- assets/javascript/wifihero.js
+++ assets/javascript/wifihero.js
@@ -2,23 +2,12 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src',"https://code.jquery.com/jquery-3.4.1.min.js");
 document.head.appendChild(jQueryScript);
 
-//var queryUrl1 = "https://api.wigle.net/api/v2/profile/AID3aa44685dab3e53850838bf8aeecdf50:efa4e947d0df628c21d6ddcd62c58ab1"
-
-//var mixDataSearch = "http://www.mapquestapi.com/search/v2/search?key=05kGEvPvXnmEAEYmQ1LFJQOng6f3ECA4&maxMatches=10&shapePoints=40.099998,-76.305603&hostedData=mqap.ntpois|group_sic_code=?|799972&remoteData=1,point,40.099998,-76.305603&remoteData=2,linestring,40.099998,-76.305603,40.199998,-76.305603,40.199998,-76.405603"
-
-//var staticMap = "https://www.mapquestapi.com/staticmap/v5/map?key=05kGEvPvXnmEAEYmQ1LFJQOng6f3ECA4&center=Atlanta,GA&size=600,400@2x"
-
-//var icons = "https://assets.mapquestapi.com/icon/v2/marker-7B0099@2x.png"
-
-//Mix Data Search/ Search Api (line 7)
-
-
 
 // I CHANGED THIS
 $.ajax({
     url: "http://www.mapquestapi.com/search/v2/search?key=05kGEvPvXnmEAEYmQ1LFJQOng6f3ECA4&maxMatches=10&shapePoints=40.099998,-76.305603&hostedData=mqap.ntpois|group_sic_code=?|799972&remoteData=1,point,40.099998,-76.305603&remoteData=2,linestring,40.099998,-76.305603,40.199998,-76.305603,40.199998,-76.405603",
     method: "GET"
-})
+});
 
 
 $("button").on("click", function() {
@@ -28,15 +17,17 @@ var allTogether = queryUrl1 + mixDataSearch + staticMap + icons;
 var results = [];
 //Wigle Api (Line 5)
 $.ajax({
-    url: "https://api.wigle.net/api/v2/profile/AID3aa44685dab3e53850838bf8aeecdf50:efa4e947d0df628c21d6ddcd62c58ab1",
+    url: "https://api.wigle.net/api/v2/network/detail/netid/operator/lac/cid/type/system/network/basestation/profile/AID3aa44685dab3e53850838bf8aeecdf50:efa4e947d0df628c21d6ddcd62c58ab1",
     method: "GET"
-  })
+  });
+console.log(response);
+
 
 //Static map api (Line 9)
   $.ajax({
       url:"https://www.mapquestapi.com/staticmap/v5/map?key=05kGEvPvXnmEAEYmQ1LFJQOng6f3ECA4&center=Atlanta,GA&size=600,400@2x",
       method:"GET" 
-  })
+  });
 //Icons Api (Line 11)
   $.ajax({
       url: "https://assets.mapquestapi.com/icon/v2/marker-7B0099@2x.png",
@@ -71,7 +62,7 @@ $.ajax({
 
 
 
-}
+});
 
 
 
