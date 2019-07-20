6a7df8d11915748a7dbbc40ea24128cedf1a6029
Jeffrey Layfield <jlayfield85@gmail.com>
2019-07-20 11:26:17 -0400

    First upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// http://www.mapquestapi.com/search/v2/radius
+
+// Example request http://www.mapquestapi.com/search/v2/radius?key=KEY&maxMatches=4&origin=39.750307,-104.999472
+
+// Mapquest Search API URL example: https://www.mapquestapi.com/search/v2/radius?origin=atlanta,ga&radius=5&maxMatches=10&ambiguities=ignore&hostedData=mqap.ntpois%7Cgroup_sic_code=?%7C581228&outFormat=json&key=KEY
+
+var mapQuest = "https://www.mapquestapi.com/search/v2/radius?origin=atlanta,ga&radius=5&maxMatches=10&ambiguities=ignore&hostedData=mqap.ntpois%7Cgroup_sic_code=?%7C581228&outFormat=json&key=KEY";
+
+var wigleApi = "https://api.wigle.net/api/v2/profile/AID3aa44685dab3e53850838bf8aeecdf50:efa4e947d0df628c21d6ddcd62c58ab1";
